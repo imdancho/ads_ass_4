b2d74b6505e8edd3d002838d2cafa194c8ef29f7
imdancho <daniyar.0586@gmail.com>
2023-05-15 03:15:11 +0600

    ads_ass_4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+
+class MyTestingClass  {
+    private int V;
+    public MyTestingClass(int V) {
+        this.V = V;
+    }
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(V);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyHashTable myHashTable = new MyHashTable<MyTestingClass, String>();
+        Random random = new Random();
+
+        for (int i = 10000; i > 0; i++) {
+
+            int V = random.nextInt(0, 10000);
+            myHashTable.put(new MyTestingClass(V), "Student" + V);
+        }
     }
 }
